c35fe0af999290c013645523c442501f13a63b6c
VanishMax <maxkors2014@gmail.com>
2019-04-04 00:57:30 +0300

    Base styles support
    And the nice Link component

--- src/Header/index.js
+++ src/Header/index.js
@@ -3,6 +3,8 @@ import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import { Link as RouterLink } from 'react-router-dom'
+import Link from '@material-ui/core/Link'
 
 class Header extends React.Component {
   render() {
@@ -12,6 +14,9 @@ class Header extends React.Component {
           <Typography variant="h4" color='inherit'>
             Пуфики
           </Typography>
+          <Link to='/lolka' component={RouterLink} variant='h6' color='inherit'>
+            Заказчики
+          </Link>
         </Toolbar>
       </AppBar>
     )
--- src/client.js
+++ src/client.js
@@ -3,18 +3,27 @@ import ReactDom from 'react-dom'
 import Routes from './routes'
 
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
-import blue from '@material-ui/core/colors/blue'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
 const theme = createMuiTheme({
   palette: {
-    primary: { main: blue[600] },
-    secondary: { main: '#7c4dff' },
+    type: 'light',
+    primary: {
+      main: '#1e88e5',
+      light: '#1e88e5',
+      dark: '#01579b'
+    },
+    secondary: {
+      main: '#7c4dff' ,
+      dark: '#311b92'
+    },
   },
   typography: { useNextVariants: true },
 });
 
 ReactDom.render(
   <MuiThemeProvider theme={theme}>
+    <CssBaseline/>
     <Routes/>
   </MuiThemeProvider>,
   document.getElementById('main'));
