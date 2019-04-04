4e13c93d15774142072871119d4b3f5f9ce4f639
VanishMax <maxkors2014@gmail.com>
2019-04-04 10:25:53 +0300

    Port for Heroku

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'public/template/index.html'));
 });
 
+const PORT = process.env.PORT || 3000;
 
-// start the server
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Server is listening in http://localhost:3000');
 });
