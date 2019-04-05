310abd9d0879e194e73ff3c3c4f0ee181a5459e9
Karen <kaskar2008@mail.ru>
2019-04-05 23:17:08 +0300

    Add common registration client

---
+++ src/modules/common/api/register.js
@@ -0,0 +1,22 @@
+import { BaseApiClient } from '#/common/api';
+
+export class RegisterAPI extends BaseApiClient {
+  constructor() {
+    super('register/');
+  }
+
+  /**
+   *
+   * @param {'customer' | 'contractor'} type
+   * @param {any} payload
+   */
+  async register(type, payload) {
+    try {
+      let result = await this.post(type);
+
+      return result;
+    } catch (e) {
+      // TODO for @kaskar2008
+    }
+  }
+}
