2b207936a7b11b983693829f7fb3acb2c95a800f
VanishMax <maxkors2014@gmail.com>
2019-04-05 21:18:29 +0300

    Edit for different roles

--- src/pages/Me/Edit.js
+++ src/pages/Me/Edit.js
@@ -12,6 +12,10 @@ import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import * as Actions from '~/redux/actions'
+
 const styles = {
   centerDiv: {
     margin: '10% auto',
@@ -71,140 +75,169 @@ const categories = [
   }
 ];
 
-function Edit() {
+class Edit extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const [values, setValues] = React.useState({
-    name: '',
-    email: '',
-    description: '',
-    password: '',
-    passwordConfirm: '',
-    select: '',
-    phone: '',
-    instagram: '',
-    tg: '',
-    vk: '',
-    fb: ''
-  });
-  const handleChange = name => event => {
-    setValues({ ...values, [name]: event.target.value });
-  };
+    this.state = {
+      name: '',
+      email: '',
+      description: '',
+      password: '',
+      passwordConfirm: '',
+      select: '',
+      phone: '',
+      instagram: '',
+      tg: '',
+      vk: '',
+      fb: ''
+    };
+  }
 
-  return(
-    <div style={styles.centerDiv}>
-      <Typography variant="h4" style={styles.title}>Подрядчик</Typography>
-      <Paper elevation={4} style={styles.paper}>
-        <Typography variant="h6" style={styles.paperTitle}>Изменение профиля</Typography>
+  handleChange(event, name) {
+    this.setState({ ...this.state, [name]: event.target.value })
+  }
 
-        <form align="center">
+  componentDidMount() {
+    if(!this.props.user) {
+      this.props.actions.chooseUser()
+    }
+  }
 
-          <ExpansionPanel style={styles.topMargin}>
-            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography color="inherit">Данные входа</Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              <TextField id="email" label="Эл. почта" type="email" name="email" style={styles.field}
-                         autoComplete="email" margin="normal" variant="outlined"
-                         value={values.email} onChange={handleChange('email')}/>
-            </ExpansionPanelDetails>
-            <ExpansionPanelDetails>
-              <TextField id="password" label="Пароль" type="password" name="password" style={styles.field}
-                         autoComplete="password" margin="normal" variant="outlined"
-                         value={values.password} onChange={handleChange('password')}/>
-            </ExpansionPanelDetails>
-            <ExpansionPanelDetails>
-              <TextField id="passwordConfirm" label="Подтверждение пароля" type="passwordConfirm"
-                         name="passwordConfirm" style={styles.field} required
-                         margin="normal" variant="outlined"
-                         value={values.passwordConfirm} onChange={handleChange('passwordConfirm')}/>
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
+  render() {
+    const values = this.state;
 
-          <ExpansionPanel>
-            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography color="inherit">Название и описание, специализация</Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              <TextField id="description" label="Описание" name="description" fullWidth
-                         autoComplete="off" margin="normal" variant="outlined" multiline
-                         value={values.description} onChange={handleChange('description')}/>
-            </ExpansionPanelDetails>
-            <ExpansionPanelDetails>
-              <TextField id="name" label="Название компании" value={values.name}
-                         name="name" onChange={handleChange('name')} margin="normal"
-                         variant="outlined" autoComplete="off" style={styles.field}/>
-            </ExpansionPanelDetails>
-            <ExpansionPanelDetails>
-              <TextField id="select" select label="Специализация" value={values.select}
-                         onChange={handleChange('select')}
-                         margin="normal" variant="outlined" style={styles.field}>
-                {categories.map(option => (
-                  <MenuItem key={option.value} value={option.value}>
-                    {option.label}
-                  </MenuItem>
-                ))}
-              </TextField>
-            </ExpansionPanelDetails>
+    return(
+      <div style={styles.centerDiv}>
+        <Typography variant="h4" style={styles.title}>
+          {this.props.user === "contractor" ? "Подрядчик": "Заказчик"}
+        </Typography>
+        <Paper elevation={4} style={styles.paper}>
+          <Typography variant="h6" style={styles.paperTitle}>Изменение профиля</Typography>
+
+          <form align="center">
+
+            <ExpansionPanel style={styles.topMargin}>
+              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography color="inherit">Данные входа</Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <TextField id="email" label="Эл. почта" type="email" name="email" style={styles.field}
+                           autoComplete="email" margin="normal" variant="outlined"
+                           value={values.email} onChange={(e) => this.handleChange(e, 'email')}/>
+              </ExpansionPanelDetails>
+              <ExpansionPanelDetails>
+                <TextField id="password" label="Пароль" type="password" name="password" style={styles.field}
+                           autoComplete="password" margin="normal" variant="outlined"
+                           value={values.password} onChange={(e) => this.handleChange(e, 'password')}/>
+              </ExpansionPanelDetails>
+              <ExpansionPanelDetails>
+                <TextField id="passwordConfirm" label="Подтверждение пароля" type="passwordConfirm"
+                           name="passwordConfirm" style={styles.field} required
+                           margin="normal" variant="outlined" value={values.passwordConfirm}
+                           onChange={(e) => this.handleChange(e, 'passwordConfirm')}/>
+              </ExpansionPanelDetails>
             </ExpansionPanel>
 
-          <ExpansionPanel>
-            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography color="inherit">Контактные данные</Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              <TextField id="phone" label="Номер телефона" value={values.phone}
-                         name="phone" onChange={handleChange('phone')} margin="normal"
-                         variant="outlined" autoComplete="phone" style={styles.field}
-                         inputProps={{maxLength: 11}} placeholder="81236547809"/>
-            </ExpansionPanelDetails>
-            <ExpansionPanelDetails>
-              <TextField id="instagram" label="Страница в Instagram" value={values.instagram} margin="normal"
-                         name="instagram" onChange={handleChange('instagram')} variant="outlined"
-                         autoComplete="off" style={styles.field} placeholder="https://www.instagram.com/spacex"/>
-            </ExpansionPanelDetails>
-            <ExpansionPanelDetails>
-              <TextField id="vk" label="Страница во Вконтакте" value={values.vk} name="vk"
-                         onChange={handleChange('vk')} margin="normal" placeholder="https://vk.com/science_technology"
-                         variant="outlined" autoComplete="off" style={styles.field}/>
-            </ExpansionPanelDetails>
-            <ExpansionPanelDetails>
-              <TextField id="tg" label="Канал в Telegram" value={values.tg} variant="outlined"
-                         name="tg" onChange={handleChange('tg')} placeholder="https://t.me/breakingmash"
-                         autoComplete="off" style={styles.field} margin="normal"/>
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
+            <ExpansionPanel>
+              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography color="inherit">Название и описание, специализация</Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <TextField id="description" label="Описание" name="description" fullWidth
+                           autoComplete="off" margin="normal" variant="outlined" multiline
+                           value={values.description} onChange={(e) => this.handleChange(e, 'description')}/>
+              </ExpansionPanelDetails>
+              <ExpansionPanelDetails>
+                <TextField id="name" label="Название компании" value={values.name}
+                           name="name" onChange={(e) => this.handleChange(e, 'name')} margin="normal"
+                           variant="outlined" autoComplete="off" style={styles.field}/>
+              </ExpansionPanelDetails>
 
-          <ExpansionPanel>
-            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography color="inherit">Фотография</Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              <input accept="image/*" style={styles.imgInput} id="buttonFile"
-                multiple type="file"/>
-              <label htmlFor="buttonFile">
-                <Button variant="outlined" color="primary" component="span" style={styles.center}>
-                  Загрузить
-                </Button>
-              </label>
-              <Typography variant="body1" color="inherit" style={styles.fileName}>
-                Название файла.png
-              </Typography>
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
+              {this.props.user === "contractor" &&
+                <ExpansionPanelDetails>
+                  <TextField id="select" select label="Специализация" value={values.select}
+                             onChange={(e) => this.handleChange(e, 'select')}
+                             margin="normal" variant="outlined" style={styles.field}>
+                    {categories.map(option => (
+                      <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                </ExpansionPanelDetails>
+              }
+
+            </ExpansionPanel>
+
+            <ExpansionPanel>
+              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography color="inherit">Контактные данные</Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <TextField id="phone" label="Номер телефона" value={values.phone}
+                           name="phone" onChange={(e) => this.handleChange(e, 'phone')} margin="normal"
+                           variant="outlined" autoComplete="phone" style={styles.field}
+                           inputProps={{maxLength: 11}} placeholder="81236547809"/>
+              </ExpansionPanelDetails>
+              <ExpansionPanelDetails>
+                <TextField id="instagram" label="Страница в Instagram" value={values.instagram} margin="normal"
+                           name="instagram" onChange={(e) => this.handleChange(e, 'instagram')} variant="outlined"
+                           autoComplete="off" style={styles.field} placeholder="https://www.instagram.com/spacex"/>
+              </ExpansionPanelDetails>
+              <ExpansionPanelDetails>
+                <TextField id="vk" label="Страница во Вконтакте" value={values.vk} name="vk"
+                           onChange={(e) => this.handleChange(e, 'vk')} margin="normal" placeholder="https://vk.com/science_technology"
+                           variant="outlined" autoComplete="off" style={styles.field}/>
+              </ExpansionPanelDetails>
+              <ExpansionPanelDetails>
+                <TextField id="tg" label="Канал в Telegram" value={values.tg} variant="outlined"
+                           name="tg" onChange={(e) => this.handleChange(e, 'tg')} placeholder="https://t.me/breakingmash"
+                           autoComplete="off" style={styles.field} margin="normal"/>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+
+            <ExpansionPanel>
+              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography color="inherit">Фотография</Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <input accept="image/*" style={styles.imgInput} id="buttonFile"
+                       multiple type="file"/>
+                <label htmlFor="buttonFile">
+                  <Button variant="outlined" color="primary" component="span" style={styles.center}>
+                    Загрузить
+                  </Button>
+                </label>
+                <Typography variant="body1" color="inherit" style={styles.fileName}>
+                  Название файла.png
+                </Typography>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
 
 
-          <Button variant="contained" color="primary" style={styles.button}>Изменить</Button>
-          <br/>
+            <Button variant="contained" color="primary" style={styles.button}>Изменить</Button>
+            <br/>
 
-          <Link to="/me" component={RouterLink} variant="subtitle1" style={styles.topMargin}>
-            Вернуться
-          </Link>
+            <Link to="/me" component={RouterLink} variant="subtitle1" style={styles.topMargin}>
+              Вернуться
+            </Link>
 
-        </form>
-      </Paper>
-
-    </div>
-  )
+          </form>
+        </Paper>
+      </div>
+    )
+  }
 }
 
-export default Edit
+const mapStateToProps = (state) => ({
+  user: state.user
+});
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators(Actions, dispatch)
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Edit)
