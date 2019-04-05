8a50d579ff77e196301cff6d8ac045cd78ca5426
VanishMax <maxkors2014@gmail.com>
2019-04-05 23:52:50 +0300

    Images for the Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,36 @@
 import React from 'react'
 
+const styles = {
+  top: {
+    height: 53,
+    paddingTop: 10,
+    boxSizing: 'content-box'
+  },
+  hackImg: {
+    height: '100%',
+    width: 'auto',
+    float: 'left',
+    marginLeft: '10%'
+  },
+  pufikiImg: {
+    height: '100%',
+    width: 'auto',
+    float: 'right',
+    marginRight: '10%'
+  },
+  middle: {
+    clear: 'both'
+  }
+};
+
 class Home extends React.Component {
   render() {
     return (
-      <main className="home">
-        <p>
-          looooool
-        </p>
+      <main>
+        <div style={styles.top}>
+          <img src="hack.png" alt="Digital Superhero logo" style={styles.hackImg}/>
+          <img src="pufiki.png" alt="Pufiki team logo" style={styles.pufikiImg}/>
+        </div>
       </main>
     )
   }
