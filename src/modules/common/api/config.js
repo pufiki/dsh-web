9b36d3d18bfae8fc38cbd959622f6576a9863fc5
Karen <dev.kaskar2008@gmail.com>
2019-04-06 17:51:51 +0300

    Add auth token from localStorage

--- src/modules/common/api/config.js
+++ src/modules/common/api/config.js
@@ -3,6 +3,6 @@ export const baseUrl = 'https://pufiki-api.herokuapp.com/';
 export const commonHeaders = {
   'Access-Content-Allow-Origin': '*',
   'Content-Type': 'application/json',
-  // Authorization: '',
+  Authorization: localStorage.getItem('authToken') || '',
   Accept: 'application/json',
 };
