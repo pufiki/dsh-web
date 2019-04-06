f3ec3a023e7e5f37999dc4b341d715422fcd2562
Karen <dev.kaskar2008@gmail.com>
2019-04-06 22:14:13 +0300

    Refactor isAuthorized HOC

--- src/HOCs/isAuthorized.js
+++ src/HOCs/isAuthorized.js
@@ -3,18 +3,22 @@ import * as Actions from '@/redux/actions'
 import { bindActionCreators } from 'redux'
 import {connect} from 'react-redux'
 
-
 export default function isAuthorizedHOC (ChildComponent) {
   class isAuthorized extends React.Component {
     constructor(props){
       super(props);
-
     }
     componentDidMount() {
-      this.props.actions.userMe();
     }
     render(){
-      return <ChildComponent user={this.props.user} {...this.props}/>
+      const childs = <ChildComponent user={this.props.user} {...this.props} />
+      const noAccess = <p>You have no access</p>
+      const loading = <p>Loading...</p>
+      if (this.props.user.isLoading) {
+        return loading
+      } else {
+        return this.props.user.info.id ? childs : noAccess
+      }
     }
   }
   return connect(
