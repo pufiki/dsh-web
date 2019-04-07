7d2cc7eb32a75a788363ffd641ebe49077e83bdb
Karen <dev.kaskar2008@gmail.com>
2019-04-07 08:51:45 +0300

    Add token check for userMe invocation

--- src/HOCs/getUserInfo.js
+++ src/HOCs/getUserInfo.js
@@ -2,7 +2,7 @@ import React from 'react'
 import * as Actions from '@/redux/actions'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-
+import { authToken } from '#/common/api/config'
 
 export default function getUserInfoHOC(ChildComponent) {
   class getUserInfo extends React.Component {
@@ -10,7 +10,7 @@ export default function getUserInfoHOC(ChildComponent) {
       super(props);
     }
     componentDidMount() {
-      this.props.actions.userMe();
+      !!authToken && this.props.actions.userMe();
     }
     render() {
       return <ChildComponent user={this.props.user} {...this.props} />
